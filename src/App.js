f24b59a1a50e55f663d0279f61cb2ccece41d7db
Juan Ignacio Salias <juanignaciosalias@gmail.com>
2023-11-02 13:51:21 -0300

    porfolio5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos:</h1>
+      <Testimonio 
+        nombre='Juana Salinas'
+        pais='Argentina'
+        imagen='emma'
+        cargo='Ingeniro'
+        empresa='Spotify'
+        testimonio='You can add webfonts, meta tags, or analytics to this file.
+        The build step will place the bundled scripts into the <body> tag.'/>
+
+<Testimonio 
+        nombre='Carla valdez'
+        pais='Argentina'
+        imagen='sarah'
+        cargo='Ingeniro'
+        empresa='Spotify'
+        testimonio='You can add webfonts, meta tags, or analytics to this file.
+        The build step will place the bundled scripts into the <body> tag.'/>
+    
+
+      <Testimonio 
+        nombre='Manuel Perez'
+        pais='Argentina'
+        imagen='shawn'
+        cargo='Ingeniro'
+        empresa='Spotify'
+        testimonio='You can add webfonts, meta tags, or analytics to this file.
+        The build step will place the bundled scripts into the <body> tag.'/>
+      </div>
+     
     </div>
   );
 }
